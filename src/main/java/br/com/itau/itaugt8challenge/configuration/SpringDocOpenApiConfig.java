2a793618365fc52f622e8f8b81f9e74688ffdc49
println <murilo_pereira@terceiros.sicredi.com.br>
2024-01-21 01:16:27 -0300

    feat: Adicionando Swagger OpenApi

---
+++ src/main/java/br/com/itau/itaugt8challenge/configuration/SpringDocOpenApiConfig.java
@@ -0,0 +1,26 @@
+package br.com.itau.itaugt8challenge.configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringDocOpenApiConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(
+                        new Info()
+                                .title("REST API - Itau Challenge")
+                                .description("API de insurance")
+                                .version("v1")
+                                .license(new License().name("Apache 2.0").url("https://www.apache.org/licenses/LICENSE-2.0"))
+                                .contact(new Contact().name("Murilo P.S").email("mu-silva@outlook.com"))
+                );
+    }
+
+}
