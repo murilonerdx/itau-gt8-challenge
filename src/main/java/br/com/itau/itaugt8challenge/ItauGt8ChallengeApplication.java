c9e157487ab977acf687bde39b5c608b9784079a
println <murilo_pereira@terceiros.sicredi.com.br>
2024-01-21 01:12:46 -0300

    feat: Adicionando projeto inicial

---
+++ src/main/java/br/com/itau/itaugt8challenge/ItauGt8ChallengeApplication.java
@@ -0,0 +1,13 @@
+package br.com.itau.itaugt8challenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItauGt8ChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItauGt8ChallengeApplication.class, args);
+    }
+
+}
